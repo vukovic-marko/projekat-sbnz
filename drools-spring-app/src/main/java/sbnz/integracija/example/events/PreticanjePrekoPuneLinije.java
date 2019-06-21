4a879bf0f66609cc6c03beebc9f302e483e13c52
Marko <vukovic.marko171@gmail.com>
2019-06-21 13:39:55 +0200

    Inicijalna implementacija preostalih cep pravila

---
+++ drools-spring-app/src/main/java/sbnz/integracija/example/events/PreticanjePrekoPuneLinije.java
@@ -0,0 +1,46 @@
+package sbnz.integracija.example.events;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.kie.api.definition.type.Expires;
+import org.kie.api.definition.type.Role;
+import org.kie.api.definition.type.Timestamp;
+
+@Role(Role.Type.EVENT)
+@Timestamp("executionTime")
+@Expires("1h")
+public class PreticanjePrekoPuneLinije implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Date executionTime;
+	private String tablice;
+	
+	public PreticanjePrekoPuneLinije(String tablice) {
+		super();
+		this.executionTime = new Date();
+		this.tablice = tablice;
+	}
+	
+	public PreticanjePrekoPuneLinije(Date executionTime, String tablice) {
+		super();
+		this.executionTime = executionTime;
+		this.tablice = tablice;
+	}
+	
+	public Date getExecutionTime() {
+		return executionTime;
+	}
+	
+	public void setExecutionTime(Date executionTime) {
+		this.executionTime = executionTime;
+	}
+	
+	public String getTablice() {
+		return tablice;
+	}
+	
+	public void setTablice(String tablice) {
+		this.tablice = tablice;
+	}
+}
--- drools-spring-app/src/test/java/cep/TestCep.java
+++ drools-spring-app/src/test/java/cep/TestCep.java
@@ -28,6 +28,7 @@ import org.kie.api.runtime.conf.ClockTypeOption;
 import static org.hamcrest.Matchers.*;
 
 import sbnz.integracija.example.events.NasilnickaVoznja;
+import sbnz.integracija.example.events.PreticanjePrekoPuneLinije;
 //import sbnz.integracija.example.events.KretanjePrekoNeisprekidaneLinije;
 //import sbnz.integracija.example.events.NasilnickaVoznja;
 import sbnz.integracija.example.events.ProlazakKrozCrvenoSvetlo;
@@ -36,7 +37,6 @@ public class TestCep {
 
 	public static KieSession kSession;
 	public static SessionPseudoClock clock;
-//	public static List<NasilnickaVoznja> lista;
 
     @Before
     public void setUpBeforeClass() throws Exception {        
@@ -51,9 +51,6 @@ public class TestCep {
 	    
 		
 		kSession = kContainer.newKieBase(config).newKieSession(kConfig, null);
-
-//        lista = new ArrayList<>();
-//        kSession.setGlobal("lista", lista);
     }
 
     @After
@@ -69,9 +66,13 @@ public class TestCep {
     @Test
     public void test() {
     	clock = kSession.getSessionClock();
-//    	List<Date> dates = new ArrayList<Date>();
     	List<NasilnickaVoznja> dates = new ArrayList<NasilnickaVoznja>();
+    	List<NasilnickaVoznja> dates2 = new ArrayList<NasilnickaVoznja>();
+    	List<NasilnickaVoznja> dates3 = new ArrayList<NasilnickaVoznja>();
+
     	kSession.setGlobal("lista", dates);
+    	kSession.setGlobal("pravilo2", dates2);
+    	kSession.setGlobal("pravilo3", dates3);
         //LocalDateTime.ofInstant(Instant.ofEpochMilli(clock.getCurrentTime()), ZoneId.systemDefault())
 
         ProlazakKrozCrvenoSvetlo e = new ProlazakKrozCrvenoSvetlo(new Date(clock.getCurrentTime()),"NS123");
@@ -103,35 +104,12 @@ public class TestCep {
         kSession.fireAllRules();
         kSession.getAgenda().getAgendaGroup("modul2").setFocus();
                 
-//        for (int i = 0; i < dates.size(); i++) {
-//        	for (int j = 0; j < dates.size(); j++) {
-//        		if (i == j) {
-//        			continue;
-//        		}
-//        		
-//        		if (dates.get(i) == dates.get(j)) {
-//        			dates.remove(i);
-//        		}
-//        	}
-//        }
-//        for (int i = 0; i < dates.size(); i++) {
-//        	for (int j = 0; j < dates.size(); j++) {
-//        		if (i == j) {
-//        			continue;
-//        		}
-//        		
-//        		if (dates.get(i).getPrekrsaji().get(0).equals(dates.get(j).getPrekrsaji().get(0))) {
-//        			dates.remove(i);
-//        		}
-//        	}
-//        }
         for (int i = 0; i < dates.size(); i++) {
         	for (int j = 0; j < dates.size(); j++) {
         		if (i == j) {
         			continue;
         		}
         		
-        		//if (dates.get(i).getPrekrsaji().get(0).equals(dates.get(j).getPrekrsaji().get(0))) {
         		if (dates.get(i).getTablice().equals(dates.get(j).getTablice()) && dates.get(j).getPrekrsaji().containsAll(dates.get(i).getPrekrsaji())) {
         			System.out.println("removing: " + dates.get(i).getPrekrsaji());
         			dates.remove(i);
@@ -151,25 +129,12 @@ public class TestCep {
         kSession.fireAllRules();
         kSession.getAgenda().getAgendaGroup("modul2").setFocus();
                 
-//        for (int i = 0; i < dates.size(); i++) {
-//        	for (int j = 0; j < dates.size(); j++) {
-//        		if (i == j) {
-//        			continue;
-//        		}
-//        		
-//        		if (dates.get(i).getPrekrsaji().get(0).equals(dates.get(j).getPrekrsaji().get(0))) {
-//        			dates.remove(i);
-//        		}
-//        	}
-//        }
-        
         for (int i = 0; i < dates.size(); i++) {
         	for (int j = 0; j < dates.size(); j++) {
         		if (i == j) {
         			continue;
         		}
         		
-        		//if (dates.get(i).getPrekrsaji().get(0).equals(dates.get(j).getPrekrsaji().get(0))) {
         		if (dates.get(i).getTablice().equals(dates.get(j).getTablice()) && dates.get(j).getPrekrsaji().containsAll(dates.get(i).getPrekrsaji())) {
         			System.out.println("removing: " + dates.get(i).getPrekrsaji());
         			dates.remove(i);
@@ -195,7 +160,6 @@ public class TestCep {
         			continue;
         		}
         		
-        		//if (dates.get(i).getPrekrsaji().get(0).equals(dates.get(j).getPrekrsaji().get(0))) {
         		if (dates.get(i).getTablice().equals(dates.get(j).getTablice()) && dates.get(j).getPrekrsaji().containsAll(dates.get(i).getPrekrsaji())) {
         			System.out.println("removing: " + dates.get(i).getPrekrsaji());
         			dates.remove(i);
@@ -209,5 +173,116 @@ public class TestCep {
         
         assertThat(2, is(dates.size()));
 
+    }
+    
+    @Test
+    public void test2() {
+    	clock = kSession.getSessionClock();
+    	List<NasilnickaVoznja> dates = new ArrayList<NasilnickaVoznja>();
+    	List<NasilnickaVoznja> dates2 = new ArrayList<NasilnickaVoznja>();
+    	List<NasilnickaVoznja> dates3 = new ArrayList<NasilnickaVoznja>();
+
+    	kSession.setGlobal("lista", dates);
+    	kSession.setGlobal("pravilo2", dates2);
+    	kSession.setGlobal("pravilo3", dates3);
+        //LocalDateTime.ofInstant(Instant.ofEpochMilli(clock.getCurrentTime()), ZoneId.systemDefault())
+
+        ProlazakKrozCrvenoSvetlo e = new ProlazakKrozCrvenoSvetlo(new Date(clock.getCurrentTime()),"NS123");
+        kSession.insert(e);
+
+        kSession.fireAllRules();
+        kSession.getAgenda().getAgendaGroup("modul2").setFocus();
+
+        System.err.println(dates2.size());
+        assertThat(0, is(dates2.size()));
+        
+        clock.advanceTime(15, TimeUnit.MINUTES);
+        ProlazakKrozCrvenoSvetlo e2 = new ProlazakKrozCrvenoSvetlo(new Date(clock.getCurrentTime()),"NS123");
+        kSession.insert(e2);
+
+        kSession.fireAllRules();
+        kSession.getAgenda().getAgendaGroup("modul2").setFocus();
+        
+        System.err.println(dates2.size());
+        
+        assertThat(0, is(dates2.size()));
+
+        
+        clock.advanceTime(11, TimeUnit.MINUTES);
+        ProlazakKrozCrvenoSvetlo e1 = new ProlazakKrozCrvenoSvetlo(new Date(clock.getCurrentTime()),"NS123");
+        kSession.insert(e1);
+
+        kSession.fireAllRules();
+        kSession.getAgenda().getAgendaGroup("modul2").setFocus();
+                
+        for (int i = 0; i < dates2.size(); i++) {
+        	for (int j = 0; j < dates2.size(); j++) {
+        		if (i == j) {
+        			continue;
+        		}
+        		
+        		if (dates2.get(i).getTablice().equals(dates2.get(j).getTablice()) && dates2.get(j).getPrekrsaji().containsAll(dates2.get(i).getPrekrsaji())) {
+        			System.out.println("removing: " + dates2.get(i).getPrekrsaji());
+        			dates2.remove(i);
+        		}
+        	}
+        }
+        
+        System.err.println(dates2.size());
+        System.err.println(dates2.get(0).getPrekrsaji());
+        
+        assertThat(1, is(dates2.size()));
+        
+        clock.advanceTime(20, TimeUnit.MINUTES);
+        ProlazakKrozCrvenoSvetlo e3 = new ProlazakKrozCrvenoSvetlo(new Date(clock.getCurrentTime()),"NS123");
+        kSession.insert(e3);
+
+        kSession.fireAllRules();
+        kSession.getAgenda().getAgendaGroup("modul2").setFocus();
+                
+        for (int i = 0; i < dates2.size(); i++) {
+        	for (int j = 0; j < dates2.size(); j++) {
+        		if (i == j) {
+        			continue;
+        		}
+        		
+        		if (dates2.get(i).getTablice().equals(dates2.get(j).getTablice()) && dates2.get(j).getPrekrsaji().containsAll(dates2.get(i).getPrekrsaji())) {
+        			System.out.println("removing: " + dates2.get(i).getPrekrsaji());
+        			dates2.remove(i);
+        		}
+        	}
+        }
+        
+        System.err.println(dates2.size());
+        System.err.println(dates2.get(0).getPrekrsaji());
+        
+        assertThat(1, is(dates2.size()));
+    }
+    
+    @Test
+    public void test3() {
+    	clock = kSession.getSessionClock();
+    	List<NasilnickaVoznja> dates = new ArrayList<NasilnickaVoznja>();
+    	List<NasilnickaVoznja> dates2 = new ArrayList<NasilnickaVoznja>();
+    	List<NasilnickaVoznja> dates3 = new ArrayList<NasilnickaVoznja>();
+    	kSession.setGlobal("lista", dates);
+    	kSession.setGlobal("pravilo2", dates2);
+    	kSession.setGlobal("pravilo3", dates3);
+
+    	PreticanjePrekoPuneLinije e1 = new PreticanjePrekoPuneLinije(new Date(clock.getCurrentTime()), "NS123");
+    	kSession.insert(e1);
+    	kSession.fireAllRules();
+    	
+    	assertThat(1, is(dates3.size()));
+    	
+    	kSession.getAgenda().getAgendaGroup("modul2").setFocus();
+    	
+    	clock.advanceTime(5, TimeUnit.MINUTES);
+    	
+    	PreticanjePrekoPuneLinije e2 = new PreticanjePrekoPuneLinije(new Date(clock.getCurrentTime()), "NS124");
+    	kSession.insert(e2);
+    	kSession.fireAllRules();
+    	
+    	assertThat(2, is(dates3.size()));
     }
 }
