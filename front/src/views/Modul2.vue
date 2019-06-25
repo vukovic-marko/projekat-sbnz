1f87bc9f35038053d0e782580ab4f6f895d12221
Marko <vukovic.marko171@gmail.com>
2019-06-25 12:33:05 +0200

    Dovršen front-end deo integracije drugog modula

--- front/src/views/Modul2.vue
+++ front/src/views/Modul2.vue
@@ -15,82 +15,91 @@
         </div>
     </form>
 
+    <p class="lead">
+      Spiskovi vozila koje je potrebno isključiti iz saobraćaja radi izdavanja njihovim vozačima kazne za nasilničku vožnju.
+    </p>
+
     <div class="container">
       <div class="row">
         <div class="col">
           <table class="table table-bordered">
-            <tr>
-              <th>Tablice</th><th>Vremena</th>
-            </tr>
-            <tr v-for="d in dates">
+            <thead>
+              <tr>
+                <th colspan="3">Vozila koja su dva ili više puta u roku od 10 minuta prošla svetlosni saobraćajni znak kada mu je tim znakom zabranjen prolaz</th>
+              </tr>
+              <tr>
+                <th>Tablice</th><th colspan="2">Vremena</th>
+              </tr>
+            </thead>
+            <tr v-for="(d,idx) in dates" :key="d">
               <td>{{d.tablice}}</td>
               <td>
-                <div v-for="p in d.prekrsaji">
+                <div v-for="p in d.prekrsaji" :key="p">
                   {{ new Date(p) | formatDate }}
                 </div>
+              </td>
+              <td>
+                <button type="button" v-on:click="remove(idx)" class="close" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
               </td>
             </tr>
           </table>
         </div>
+
         <div class="col">
           <table class="table table-bordered">
-            <tr>
-              <th>Tablice</th><th>Vremena</th>
-            </tr>
-            <tr v-for="d in dates2">
+            <thead>
+              <tr>
+                <th colspan="3">Vozila koja su tri ili više puta u roku od jednog sata prošla svetlosni saobraćajni znak kada mu je tim znakom zabranjen prolaz</th>
+              </tr>
+              <tr>
+                <th>Tablice</th><th colspan="2">Vremena</th>
+              </tr>
+            </thead>
+            <tr v-for="(d,idx) in dates2" :key="d">
               <td>{{d.tablice}}</td>
               <td>
-                <div v-for="p in d.prekrsaji">
+                <div v-for="p in d.prekrsaji" :key="p">
                   {{ new Date(p) | formatDate }}
                 </div>
+              </td>
+              <td>
+                <button type="button" v-on:click="remove2(idx)" class="close" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
               </td>
             </tr>
           </table>
         </div>
+
         <div class="col">
           <table class="table table-bordered">
-            <tr>
-              <th>Tablice</th><th>Vremena</th>
-            </tr>
-            <tr v-for="d in dates3">
+            <thead>
+              <tr>
+                <th colspan="3">Vozila koja su se kretala preko neisprekidane uzdužne linije koja razdvaja kolovozne trake po smerovima kretanja</th>
+              </tr>
+              <tr>
+                <th>Tablice</th><th colspan="2">Vremena</th>
+              </tr>
+            </thead>
+            <tr v-for="(d,idx) in dates3" :key="d">
               <td>{{d.tablice}}</td>
               <td>
-                <div v-for="p in d.prekrsaji">
+                <div v-for="p in d.prekrsaji" :key="p">
                   {{ new Date(p) | formatDate }}
                 </div>
+              </td>
+              <td>
+                <button type="button" v-on:click="remove3(idx)" class="close" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
               </td>
             </tr>
           </table>
         </div>
       </div>
     </div>
-    
-
-    
-
-   
-
-    <!-- <div v-for="d in dates">
-      {{ d.tablice }}
-      <div v-for="p in d.prekrsaji">
-        {{ new Date(p) | formatDate }}
-      </div>
-    </div>
-    ---
-    <div v-for="d in dates2">
-      {{ d.tablice }}
-      <div v-for="p in d.prekrsaji">
-        {{ new Date(p) | formatDate }}
-      </div>
-    </div>
-    ---
-    <div v-for="d in dates3">
-      {{ d.tablice }}
-      <div v-for="p in d.prekrsaji">
-        {{ new Date(p) | formatDate }}
-      </div>
-    </div> -->
-
   </div>
 </template>
 
@@ -98,7 +107,6 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import axios from 'axios';
-
 
 export default {
   name: 'modul2',
@@ -110,12 +118,14 @@ export default {
 
       if (this.$data.forma.tablice != null && this.$data.forma.tablice != '') {
         axios.post('http://localhost:8080/nasilnicka', this.$data.forma)
+        // eslint-disable-next-line
         .then(response => {
           alert('Prekršaj uspešno evidentiran!')
           // this.$router.go()
           this.fun()
           
         })
+        // eslint-disable-next-line
         .catch(e => {
           alert('Evidentiranje prekršaja nije uspelo!')
         })
@@ -137,6 +147,27 @@ export default {
     axios.get('http://localhost:8080/nasilnicka/dates3')
       .then(response => {
         this.$data.dates3 = response.data
+      })
+    },
+    remove: function(idx) {
+      axios.delete('http://localhost:8080/nasilnicka/dates/'+idx)
+      // eslint-disable-next-line
+      .then(response => {
+        this.fun()
+      })
+    },
+    remove2: function(idx) {
+       axios.delete('http://localhost:8080/nasilnicka/dates2/'+idx)
+      // eslint-disable-next-line
+      .then(response => {
+        this.fun()
+      })
+    },
+    remove3: function(idx) {
+       axios.delete('http://localhost:8080/nasilnicka/dates3/'+idx)
+      // eslint-disable-next-line
+      .then(response => {
+        this.fun()
       })
     }
   }, data: function() {
@@ -160,7 +191,8 @@ export default {
 .container {
   margin-top: 1em; 
 }
-/* table, th, td {
-  border: 1px solid black;
-} */
+.lead {
+  margin-top: 1em;
+  margin-bottom: 2em;
+}
 </style>
